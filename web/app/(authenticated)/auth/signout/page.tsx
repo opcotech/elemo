a34bb0456c704f25fc7c32a387a2f37e01bf35f8
Gabor Boros <gabor@opcotech.com>
2023-06-06 08:20:13 +0200

    refactor: adjust signout page

--- web/app/(authenticated)/auth/signout/page.tsx
+++ web/app/(authenticated)/auth/signout/page.tsx
@@ -4,8 +4,8 @@ import { SignOutForm } from '@/components/auth/SignOutForm';
 
 export default function SignOutPage() {
   return (
-    <div className="h-screen w-screen flex items-center">
-      <div className="max-w-xl mx-auto text-center">
+    <div className="h-full w-screen flex items-center">
+      <div className="max-w-xl mx-auto px-6 text-center">
         <h2 className="mb-4">Are you sure you want to sign out?</h2>
         <p className="mb-10">
           By signing out, your session will be terminated and you will be redirected to login page.
