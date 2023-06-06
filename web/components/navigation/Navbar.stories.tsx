b964dd1fbeeebcdd6e1e08f565e3c77618fd104b
Gabor Boros <gabor@opcotech.com>
2023-06-06 08:44:38 +0200

    chore: add navbar stories

---
+++ web/components/navigation/Navbar.stories.tsx
@@ -0,0 +1,84 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Navbar } from './Navbar';
+import { SessionProvider } from 'next-auth/react';
+import { Session } from 'next-auth';
+
+const FAKE_SESSION: Session = {
+  user: {
+    id: '1',
+    name: 'John Doe',
+    email: 'john.doe@example.com',
+    image: 'https://picsum.photos/id/433/100/100?w=256&q=75',
+    access_token: '',
+    refresh_token: '',
+    expires_in: 3600
+  },
+  accessToken: '',
+  expires: new Date().toISOString()
+};
+
+const meta: Meta<typeof Navbar> = {
+  title: 'Navigation/Navbar',
+  component: Navbar,
+  tags: ['autodocs'],
+  render: (args) => (
+    <SessionProvider session={FAKE_SESSION}>
+      <Navbar {...args} />
+    </SessionProvider>
+  )
+};
+
+export default meta;
+type Story = StoryObj<typeof Navbar>;
+
+export const Sample: Story = {
+  args: {
+    hasTodos: false,
+    hasNotifications: false,
+    navigation: [
+      { id: '1', href: '#', label: 'Dashboard', prefetch: false },
+      { id: '1', href: '#', label: 'Users', prefetch: false },
+      { id: '1', href: '#', label: 'Settings', prefetch: false }
+    ],
+    userNavigation: [
+      { id: '1', href: '#', label: 'Profile', prefetch: false },
+      { id: '1', href: '#', label: 'Settings', prefetch: false },
+      { id: '1', href: '#', label: 'Sign out', prefetch: false }
+    ]
+  }
+};
+
+export const HasTodos: Story = {
+  args: {
+    hasTodos: true,
+    hasNotifications: false,
+    navigation: [
+      { id: '1', href: '#', label: 'Dashboard', prefetch: false },
+      { id: '1', href: '#', label: 'Users', prefetch: false },
+      { id: '1', href: '#', label: 'Settings', prefetch: false }
+    ],
+    userNavigation: [
+      { id: '1', href: '#', label: 'Profile', prefetch: false },
+      { id: '1', href: '#', label: 'Settings', prefetch: false },
+      { id: '1', href: '#', label: 'Sign out', prefetch: false }
+    ]
+  }
+};
+
+export const HasNotifications: Story = {
+  args: {
+    hasTodos: false,
+    hasNotifications: true,
+    navigation: [
+      { id: '1', href: '#', label: 'Dashboard', prefetch: false },
+      { id: '1', href: '#', label: 'Users', prefetch: false },
+      { id: '1', href: '#', label: 'Settings', prefetch: false }
+    ],
+    userNavigation: [
+      { id: '1', href: '#', label: 'Profile', prefetch: false },
+      { id: '1', href: '#', label: 'Settings', prefetch: false },
+      { id: '1', href: '#', label: 'Sign out', prefetch: false }
+    ]
+  }
+};
