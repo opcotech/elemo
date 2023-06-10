19cd641dcd430c2b7dfd4e89c5466d24c76d19b7
Gabor Boros <gabor@opcotech.com>
2023-06-10 08:07:32 +0200

    fix: temporarily disable nextjs link navigation

--- web/components/blocks/Link.tsx
+++ web/components/blocks/Link.tsx
@@ -1,4 +1,3 @@
-import { default as NextLink } from 'next/link';
 import type { AnchorHTMLAttributes } from 'react';
 
 import { concat } from '@/lib/helpers';
@@ -9,7 +8,19 @@ export interface LinkProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
 }
 
 export function Link({ href = '#', prefetch = true, decorated = true, className, children, ...props }: LinkProps) {
-  const Component = href.startsWith('#') ? 'a' : NextLink;
+  /*
+   * FIXME: This component is disabling prefetching for all links, regardless of
+   *  the value of the `prefetch` prop. This is because the `prefetch` prop is
+   *  storing the resulting page on client-side cache, but the cache is not being
+   *  updated when a dynamic route is used. This is a known issue with Next.js:
+   *   - https://github.com/vercel/next.js/issues/42991#issuecomment-1517828363
+   *  The intended proper workaround would be to use `revalidatePath`, but that
+   *  has a bug that prevents client-side cache from being purged if not called
+   *  from the same page that is being updated:
+   *   - https://github.com/vercel/next.js/issues/42991#issuecomment-1567610032
+   * */
+  // const Component = href.startsWith('#') ? 'a' : NextLink;
+  const Component = 'a';
 
   return (
     <Component
