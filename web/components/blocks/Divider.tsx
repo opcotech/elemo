7b9655de576b0ed9b7c39da1fb4d22aec49ed5d1
Gabor Boros <gabor@opcotech.com>
2023-06-10 08:06:35 +0200

    refactor: extend divider capabilities

--- web/components/blocks/Divider.tsx
+++ web/components/blocks/Divider.tsx
@@ -1,6 +1,12 @@
-export function Divider() {
+import { concat } from '@/lib/helpers';
+
+export interface DividerProps {
+  className?: string;
+}
+
+export function Divider({ className }: DividerProps) {
   return (
-    <div className="relative">
+    <div className={concat('relative', className)}>
       <div className="absolute inset-0 flex items-center" aria-hidden="true">
         <div className="w-full border-t border-gray-200" />
       </div>
