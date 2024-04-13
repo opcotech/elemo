53fedc98ff9344a531002a2600d5cc4ad54633ec
Gabor Boros <gabor@opcotech.com>
2024-04-13 12:52:38 +0400

    test: check for system health response in smoke tests
    Signed-off-by: Gabor Boros <gabor@opcotech.com>

--- tests/load/scripts/system.js
+++ tests/load/scripts/system.js
@@ -4,15 +4,18 @@ import {check, group} from 'k6';
 import {deepEqual} from '../common/utils.js';
 
 export function getSystemHealth() {
-  group('System', function () {
+  group('system', function () {
     const res = http.get(`${globalThis.BASE_URL}/v1/system/health`);
 
     check(res, {
       'status is 200': r => r.status === 200,
       'protocol is HTTP/2': r => r.proto === 'HTTP/2.0',
       'response body': r => deepEqual(JSON.parse(r.body), {
-        "graph_database": "healthy",
-        "relational_database": "healthy"
+          'cache_database': 'healthy',
+          'graph_database': 'healthy',
+          'license': 'healthy',
+          'message_queue': 'healthy',
+          'relational_database': 'healthy',
       }),
     });
   });
