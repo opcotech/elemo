cdd758443e5f9947204528d0d646441a60fd6368
Gabor Boros <gabor@opcotech.com>
2024-04-15 22:02:08 +0400

    style: reformat code
    Signed-off-by: Gabor Boros <gabor@opcotech.com>

--- web/packages/elemo-client/schemas/$SystemHealth.ts
+++ web/packages/elemo-client/schemas/$SystemHealth.ts
@@ -3,26 +3,26 @@
 /* tslint:disable */
 /* eslint-disable */
 export const $SystemHealth = {
-    properties: {
-        cache_database: {
-            type: 'Enum',
-            isRequired: true,
-        },
-        graph_database: {
-            type: 'Enum',
-            isRequired: true,
-        },
-        relational_database: {
-            type: 'Enum',
-            isRequired: true,
-        },
-        license: {
-            type: 'Enum',
-            isRequired: true,
-        },
-        message_queue: {
-            type: 'Enum',
-            isRequired: true,
-        },
+  properties: {
+    cache_database: {
+      type: 'Enum',
+      isRequired: true
     },
+    graph_database: {
+      type: 'Enum',
+      isRequired: true
+    },
+    relational_database: {
+      type: 'Enum',
+      isRequired: true
+    },
+    license: {
+      type: 'Enum',
+      isRequired: true
+    },
+    message_queue: {
+      type: 'Enum',
+      isRequired: true
+    }
+  }
 } as const;
