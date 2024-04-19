d976979376df523509268f4747ea6bf09df78251
Gabor Boros <gabor@opcotech.com>
2024-04-19 06:59:29 +0400

    chore: ensure commit lint ignores dependencies
    Signed-off-by: Gabor Boros <gabor@opcotech.com>

---
+++ .commitlintrc.mjs
@@ -0,0 +1,24 @@
+const configuration = {
+  /*
+   * Resolve and load @commitlint/config-conventional from node_modules.
+   * Referenced packages must be installed
+   */
+  extends: ["@commitlint/config-conventional"],
+  /*
+   * Array of functions that return true if commitlint should ignore the given message.
+   * Given array is merged with predefined functions, which consist of matchers like:
+   *
+   * - 'Merge pull request', 'Merge X into Y' or 'Merge branch X'
+   * - 'Revert X'
+   * - 'v1.2.3' (ie semver matcher)
+   * - 'Automatic merge X' or 'Auto-merged X into Y'
+   *
+   * To see full list, check https://github.com/conventional-changelog/commitlint/blob/master/%40commitlint/is-ignored/src/defaults.ts.
+   * To disable those ignores and run rules always, set `defaultIgnores: false` as shown below.
+   */
+  ignores: [
+    (message) => /^chore\(deps\): bump (.*) from (.*) to (.*)$/m.test(message),
+  ],
+};
+
+export default configuration;
