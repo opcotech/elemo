f0e552dc0eb269b4b08ffecdf73ff41afde6ea09
Gabor Boros <gabor@opcotech.com>
2024-04-20 14:36:31 +0400

    feat: setup notification DB table
    Signed-off-by: Gabor Boros <gabor@opcotech.com>

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,14 +3,43 @@
 set -euo pipefail
 
 ROOT_DIR="$(realpath "$(dirname "${BASH_SOURCE[0]:-$0}")/..")"
-source "${ROOT_DIR}/scripts/common.sh";
+CMD_DIR="${ROOT_DIR}/cmd/elemo"
+DEV_CONFIG_DIR="${ROOT_DIR}/configs/development"
+DOCKER_DEPLOY_DIR="${ROOT_DIR}/deploy/docker"
+QUERIES_DIR="${ROOT_DIR}/assets/queries"
+SCRIPTS_DIR="${ROOT_DIR}/scripts"
+WEB_DIR="${ROOT_DIR}/web"
+
+export ELEMO_CONFIG="${DEV_CONFIG_DIR}/config.local.gen.yml"
+
+function checkInstalled() {
+  local program="${1}"
+
+  if ! type "${program}"; then
+      echo "Couldn't find ${program} in your PATH. Make sure it is installed."
+      exit 1
+  fi
+}
+
+function waitAndPrint() {
+  echo "waiting ${1} seconds to let the services boot"
+  sleep "${1}"
+}
+
+function backupCopyFile() {
+  local backupFile="${1}"
+  local copyFile="${2}"
+
+  [[ -f "${backupFile}" ]] && mv "${backupFile}" "${backupFile}.bkp"
+  cp "${copyFile}" "${backupFile}"
+}
 
 function setupOAuthClient() {
-  log "setting up OAuth2 client"
-  ADD_CLIENT_OUT=$(docker compose \
-    -f "${DOCKER_DEPLOY_DIR}/docker-compose.yml" exec -T elemo-server bin/elemo auth add-client \
-        --callback-url http://127.0.0.1:3000/api/auth/callback/elemo --public 2>&1 | grep "client-id")
+  # Add Oauth2 client to DB
+  go run "${CMD_DIR}/main.go" auth add-client --callback-url http://127.0.0.1:3000/api/auth/callback/elemo --public 2>&1
+  ADD_CLIENT_OUT=$(go run "${CMD_DIR}/main.go" auth add-client --callback-url http://127.0.0.1:3000/api/auth/callback/elemo --public 2>&1 | grep "client-id")
 
+  # Add generated client secrets to frontend config
   backupCopyFile "${WEB_DIR}/.env" "${WEB_DIR}/.env.example"
   backupCopyFile "${WEB_DIR}/.env.test.local" "${WEB_DIR}/.env.test.example"
 
@@ -20,10 +49,18 @@ function setupOAuthClient() {
 }
 
 function setupDemoData() {
-  log "loading demo data"
-  echo "MATCH (n) DETACH DELETE n" | docker compose -f "${DOCKER_DEPLOY_DIR}/docker-compose.yml" exec -T neo4j cypher-shell -u "neo4j" -p "neo4jsecret"
-  docker compose -f "${DOCKER_DEPLOY_DIR}/docker-compose.yml" exec -T neo4j cypher-shell -u "neo4j" -p "neo4jsecret" < "${QUERIES_DIR}/bootstrap.cypher"
-  docker compose -f "${DOCKER_DEPLOY_DIR}/docker-compose.yml" exec -T neo4j cypher-shell -u "neo4j" -p "neo4jsecret" < "${QUERIES_DIR}/demo.cypher"
+  # Spin up services
+  docker-compose -f "${DOCKER_DEPLOY_DIR}/docker-compose.yml" up neo4j --remove-orphans -d
+  docker-compose -f "${DOCKER_DEPLOY_DIR}/docker-compose.yml" up postgres --remove-orphans -d
+  waitAndPrint 5
+
+  # Load data into Neo4J
+  echo "MATCH (n) DETACH DELETE n" | docker-compose -f "${DOCKER_DEPLOY_DIR}/docker-compose.yml" exec -T neo4j cypher-shell -u "neo4j" -p "neo4jsecret"
+  docker-compose -f "${DOCKER_DEPLOY_DIR}/docker-compose.yml" exec -T neo4j cypher-shell -u "neo4j" -p "neo4jsecret" < "${QUERIES_DIR}/bootstrap.cypher"
+  docker-compose -f "${DOCKER_DEPLOY_DIR}/docker-compose.yml" exec -T neo4j cypher-shell -u "neo4j" -p "neo4jsecret" < "${QUERIES_DIR}/demo.cypher"
+
+  # Load data into Postgres
+  docker-compose -f "${DOCKER_DEPLOY_DIR}/docker-compose.yml" exec -T postgres psql postgres://elemo:pgsecret@postgres/elemo < "${QUERIES_DIR}/bootstrap.sql"
 }
 
 function installFrontEnd() {
@@ -46,12 +83,16 @@ generateConfigIfMissing
 docker compose -f "${DOCKER_DEPLOY_DIR}/docker-compose.yml" up --remove-orphans -d
 waitAndPrint 5
 
+# Spin up services
+docker-compose -f "${DOCKER_DEPLOY_DIR}/docker-compose.yml" up neo4j postgres --remove-orphans -d
+waitAndPrint 5
+
 # Create a new OAuth2 client and configure the front-end
 setupOAuthClient
 setupDemoData
 
 # Tear down services
-docker compose -f "${DOCKER_DEPLOY_DIR}/docker-compose.yml" down
+docker-compose -f "${DOCKER_DEPLOY_DIR}/docker-compose.yml" down
 
 # Setup the front-end
 installFrontEnd
