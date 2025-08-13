b6262b37b74e35170067405cb41a96f438a973fa
zyfy29 <wasuremono127@gmail.com>
2025-08-13 15:24:49 +0900

    test: generate mock AttachmentRepository by gomock
    Signed-off-by: zyfy29 <wasuremono127@gmail.com>

--- internal/repository/attachment.go
+++ internal/repository/attachment.go
@@ -7,6 +7,8 @@ import (
 )
 
 // AttachmentRepository is a repository for managing attachments.
+//
+//go:generate mockgen -source=attachment.go -destination=../testutil/mock/attachment_repo_gen.go -package=mock -mock_names "AttachmentRepository=AttachmentRepository"
 type AttachmentRepository interface {
 	Create(ctx context.Context, belongsTo model.ID, attachment *model.Attachment) error
 	Get(ctx context.Context, id model.ID) (*model.Attachment, error)
