f7621ef8e7ff876536ea527573cf3079aaf3de73
zyfy29 <wasuremono127@gmail.com>
2025-08-13 18:52:25 +0900

    test: generate mock AssignmentRepository by gomock
    Signed-off-by: zyfy29 <wasuremono127@gmail.com>

--- internal/repository/assignment.go
+++ internal/repository/assignment.go
@@ -7,6 +7,8 @@ import (
 )
 
 // AssignmentRepository is a repository for managing resource assignments.
+//
+//go:generate mockgen -source=assignment.go -destination=../testutil/mock/assignment_repo_gen.go -package=mock -mock_names "AssignmentRepository=AssignmentRepository"
 type AssignmentRepository interface {
 	Create(ctx context.Context, assignment *model.Assignment) error
 	Get(ctx context.Context, id model.ID) (*model.Assignment, error)
