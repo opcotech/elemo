62e37e6ac74ab0face9cca486a16a0e0d9fb8787
zyfy29 <wasuremono127@gmail.com>
2025-08-20 23:10:54 +0900

    refactor: replace UserTokenRepository mock by gomock generated one (#345)
    Signed-off-by: zyfy29 <wasuremono127@gmail.com>

--- internal/repository/auth.go
+++ internal/repository/auth.go
@@ -7,6 +7,8 @@ import (
 )
 
 // UserTokenRepository is a repository for managing user tokens.
+//
+//go:generate mockgen -source=auth.go -destination=../testutil/mock/user_token_repo_gen.go -package=mock -mock_names "UserTokenRepository=UserTokenRepository"
 type UserTokenRepository interface {
 	Create(ctx context.Context, token *model.UserToken) error
 	Get(ctx context.Context, userID model.ID, tokenCtx model.UserTokenContext) (*model.UserToken, error)
